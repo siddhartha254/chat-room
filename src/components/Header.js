a8c4f8d76f44d8f94e000f88ef88b67b9477066a
Siddhartha <siddhartha.arora4@gmail.com>
2023-06-06 13:15:54 +0530

    final changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 
     return (
-
+        
         <div className="header-container">
             <p class="header-text">Welcome to the Chat Arena !</p>
         </div>
